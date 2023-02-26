7c64c3df21a30aa284294e4a71f6fa921bd8d0b2
afroozshaikh <shaikhafroozabdulla7@gmail.com>
2023-02-26 15:53:31 +0400

    NEOHACK : swagger configuration

--- src/main/java/com/project/stressAI/StressAiApplication.java
+++ src/main/java/com/project/stressAI/StressAiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@OpenAPIDefinition(info=@Info(title="Annual Day Participants"))
+@OpenAPIDefinition(info=@Info(title="Stress AI Application"))
 public class StressAiApplication {
 
 	public static void main(String[] args) {
